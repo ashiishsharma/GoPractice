643bb9fad1234289497df7794ba3e78a8a88a323
Ashish Sharma <ashish.sharma@menlosecurity.com>
2025-04-21 12:44:23 +0530

    Added more data structures

--- dataStructures/linked_list.go
+++ dataStructures/linked_list.go
@@ -1,12 +1,18 @@
 package dataStructures
 
+import (
+	"errors"
+	"iter"
+)
+
 type IntegerLinkedList interface {
 	int | int64 | float64
 }
 
 type ICustomLinkedList[T IntegerLinkedList] interface {
 	add(T)
-	remove()
+	addToIndex(T, index int) error
+	remove(index int) (T, error)
 	getSize() int
 }
 type CustomLinkedList[T IntegerLinkedList] struct {
@@ -32,8 +38,70 @@ func (customLinkedList *CustomLinkedList[T]) add(t T) {
 	customLinkedList.size++
 }
 
-func (customLinkedList *CustomLinkedList[T]) remove() {
+func (customLinkedList *CustomLinkedList[T]) addToIndex(t T, index int) error {
+	var nodeToAdd = NewNode(t, nil)
+	var err error
+	if index < 0 || index >= customLinkedList.size {
+		return errors.New("index out of range")
+	} else if customLinkedList.size == 0 {
+		return errors.New("Empty List")
+	} else if index == 0 {
+		nodeToAdd.next = customLinkedList.head
+		customLinkedList.head = nodeToAdd
+		err = nil
+	} else {
+		iterationNode := customLinkedList.head
+		for i := 0; i < index && i < customLinkedList.size; i++ {
+			if i == index-1 {
+				break
+			}
+			iterationNode = iterationNode.next
+		}
+		if iterationNode.next != nil {
+			nodeToAdd.next = iterationNode.next
+			iterationNode.next = nodeToAdd
+		} else {
+			iterationNode.next = nodeToAdd
+		}
+	}
+	if err == nil {
+		customLinkedList.size++
+	}
+	return err
+}
+
+func (customLinkedList *CustomLinkedList[T]) remove(index int) (T, error) {
+	var elementToRemove T
+	var err error
+	if index < 0 || index >= customLinkedList.size {
+		return elementToRemove, errors.New("index out of range")
+	} else if customLinkedList.size == 0 {
+		return elementToRemove, errors.New("Empty List")
+	} else if customLinkedList.size == 1 && index == 0 {
+		elementToRemove = customLinkedList.head.data
+		customLinkedList.head = nil
+		err = nil
+	} else if index == 0 {
+		elementToRemove = customLinkedList.head.data
+		customLinkedList.head = customLinkedList.head.next
+		err = nil
+	} else {
+		iterationNode := customLinkedList.head
+		for i := 0; i < index && i < customLinkedList.size; i++ {
+			if i == index-1 {
+				break
+			}
+			iterationNode = iterationNode.next
+		}
+		elementToRemove = iterationNode.next.data
+		if iterationNode.next.next != nil {
+			iterationNode.next = iterationNode.next.next
+		} else {
+			iterationNode.next = nil
+		}
+	}
 	customLinkedList.size--
+	return elementToRemove, err
 }
 
 func (customLinkedList *CustomLinkedList[T]) getSize() int {
@@ -47,4 +115,15 @@ type Node[T IntegerLinkedList] struct {
 
 func NewNode[T IntegerLinkedList](data T, next *Node[T]) *Node[T] {
 	return &Node[T]{data: data, next: next}
+}
+
+func (customLinkedList *CustomLinkedList[T]) All() (iteratorSequence iter.Seq[T]) {
+	iteratorSequence = func(yield func(T) bool) {
+		for node := customLinkedList.head; node != nil; node = node.next {
+			if !yield(node.data) {
+				return
+			}
+		}
+	}
+	return iteratorSequence
 }
--- dataStructures/linked_list_test.go
+++ dataStructures/linked_list_test.go
@@ -14,11 +14,56 @@ func TestLinkedListAdd(t *testing.T) {
 	assert.Equal(t, linkedList.getSize(), 4)
 }
 
-func TestLinkedListRemove(t *testing.T) {
+func TestLinkedListAddAtIndex(t *testing.T) {
 	var linkedList = NewCustomLinkedList[int]()
 	linkedList.add(34)
 	linkedList.add(56)
 	linkedList.add(83)
 	linkedList.add(97)
 	assert.Equal(t, linkedList.getSize(), 4)
+
+	linkedList.addToIndex(89, 0)
+	assert.Equal(t, linkedList.getSize(), 5)
+
+	linkedList.addToIndex(172, 4)
+	assert.Equal(t, linkedList.getSize(), 6)
+
+	linkedList.addToIndex(17, 3)
+	assert.Equal(t, linkedList.getSize(), 7)
+
+	for v := range linkedList.All() {
+		println(v)
+	}
+}
+
+func TestLinkedListRemove(t *testing.T) {
+	var linkedList = NewCustomLinkedList[int]()
+	linkedList.add(34)
+	linkedList.add(56)
+	linkedList.add(83)
+	linkedList.add(97)
+	linkedList.add(98)
+	linkedList.add(992)
+	assert.Equal(t, linkedList.getSize(), 6)
+
+	removedElement, err := linkedList.remove(0)
+	assert.Nil(t, err)
+	assert.Equal(t, 34, removedElement)
+
+	removedElement, err = linkedList.remove(3)
+	assert.Nil(t, err)
+	assert.Equal(t, 98, removedElement)
+
+	linkedList.add(9)
+	linkedList.add(8)
+	linkedList.add(92)
+	assert.Equal(t, linkedList.getSize(), 7)
+
+	removedElement, err = linkedList.remove(6)
+	assert.Nil(t, err)
+	assert.Equal(t, 92, removedElement)
+
+	for v := range linkedList.All() {
+		println(v)
+	}
 }
